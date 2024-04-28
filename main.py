b9686e3324b2d3f7c77e80b8d2eaa3a4cbd6bd8d
Thepyt <87644800+Thepyt@users.noreply.github.com>
2024-04-28 18:50:42 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -15,3 +15,17 @@ class App:
             Bot().message(self.message)
 
 
+while True:
+    hour = datetime.datetime.now().hour
+    min = datetime.datetime.now().min
+
+    if hour == 15 and min == 30:
+        App("omg").sendMessage()
+    elif hour == 16 and min == 45:
+        App("news").sendMessage()
+    elif hour == 0 and min == 15:
+        App("ifoss").sendMessage()
+    elif hour == 10 and min == 0:
+        App("point").sendMessage()
+    elif hour == 12 and min == 0:
+        App("925").sendMessage()
